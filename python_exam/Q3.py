f51377bb473df8d6bb45db02a402e379b3546fdf
Whitefang4 <117334153+Whitefang4@users.noreply.github.com>
2022-11-08 16:40:26 +0530

    Update Q3.py

--- python_exam/Q3.py
+++ python_exam/Q3.py
@@ -19,6 +19,29 @@ def number_of_routes(source_stopid: str, destination_stopid: str) -> int:
     final_count = -1
     try:
         # Enter your code here
+         Q = [source]
+        p = {source:None}
+        w = [0]
+        d = {}
+        for i in graph_object:
+            d[i] = float('inf')
+            Q.append(i)
+            w.append(d[i])
+        d[source] = 0
+        S =[]
+        n = len(Q)
+        final_count = 0
+        while Q:
+            u = extract(Q,w)[1]
+            s.append(u)
+            Q.remove(u)
+            for v in G[u]:
+                if d[v]>= d[u]+G[u][v]:
+                    d[v]=d[u]+G[u][v]
+                    p[v]=u
+            final_count = final_count +1
+        
+        
         return final_count
     except:
         return final_count
